a72ffdc22d4c0b9b0fea64193046e6e352e72497
Abhishek Kothari <abk.edu@gmail.com>
2019-01-06 01:48:30 +0530

    Completed UI development

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
 const genFunctions = require('./generator/mygenerator');
 const generator = require('./generator/generator');
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+var session = require('express-session');
+let generatorsMap = [];
+app.use('/public', express.static('public'));
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
 
-var express=require('express');
-var app=express();
-app.use('/public',express.static('public'));
-
-
-var server=app.listen(3000,function() {
-    console.log('Listening to port 3000');
-    let mygen = new genFunctions();
-    let gen = new generator(mygen.rangeSeq);
+var server = app.listen(3000, function () {
+    console.log('Server listening on port 3000');
 });
+//Route for normal index file
+app.get('/', function (req, resp) {
+    resp.sendFile(path.resolve(__dirname) + '/public/index.html');
+});
+//Route to initialize the session with fibonacci generator
+app.get('/fib', function (req, resp) {
+    let mygen = new genFunctions();
+    generatorsMap[req.session.id] = mygen.fibonacciSeq;
+    req.session.save();
+    resp.send('Generator Loaded');
+})
+//Route to initialize the session with factorial generator
+app.get('/fact', function (req, resp) {
+    let mygen = new genFunctions();
+    generatorsMap[req.session.id] = mygen.factorialSeq;
+    req.session.save();
+    resp.send('Generator Loaded');
+})
+//Route to initialize the session with range generator
+app.post('/range', function (req, resp) {
+    let mygen = new genFunctions();
+    generatorsMap[req.session.id] = mygen.rangeSeq;
+    //Stores the concerned attributes in session
+    req.session.initialValue = req.body.initialValue;
+    req.session.step = req.body.step;
+    req.session.save();
+    resp.send('Generator Loaded');
+})
+//Route to initialize the session with partial sum generator
+app.post('/sum', function (req, resp) {
+    let mygen = new genFunctions();
+    generatorsMap[req.session.id] = mygen.partialSumSeq;
+    //Store the attributes in session
+    req.session.partialvalues = req.body.valueArr;
+    req.session.save();
+    resp.send('Generator Loaded');
+})
+//Route to initialize the session with Prime number generator
+app.get('/prime', function (req, resp) {
+    let mygen = new genFunctions();
+    generatorsMap[req.session.id] = mygen.primeSeq;
+    req.session.save();
+    resp.send('Generator Loaded');
+})
+//Route to initialize the session with next number generator
+app.get('/next', function (req, resp) {
+    var genFn = generatorsMap[req.session.id];
+    let gen;
+    if (req.session.initialValue != undefined) {
+        gen = new generator(genFn, parseInt(req.session.initialValue), parseInt(req.session.step));
+    } else if (req.session.partialvalues != undefined) {
+
+        gen = new generator(genFn, parseInt(req.session.partialvalues));
+    } else {
+
+        gen = new generator(genFn);
+    }
+    var nextVal = '';
+    try {
+        nextVal = gen.next();
+    } catch (e) {
+        console.error(e);
+    }
+    resp.status(200).send({ next: nextVal });
+})
---
+++ public/main.js
@@ -0,0 +1,65 @@
+var generators = ['/fib','/fact','/range','/sum','/prime'];
+function activate(code){
+    var genPath = generators[code];
+    if(code==0||code==1||code==4){
+        $.get(genPath,function(data){
+            alert(data);
+            $('#activator').slideUp();
+            $('.rocket').animate({
+                top: -1000
+            },2000,function(){
+                $('#activated').slideDown();
+            });
+            
+        });
+    }else if(code==2){
+        var initialValue = $('#initialvalue').val();
+        var step = $('#step').val();
+        var data = {};
+        data.initialValue= initialValue;
+        data.step = step;
+        $.ajax({
+            type:'POST',
+            url:genPath,
+            data:JSON.stringify(data),
+            success:function(d){
+                $('#activator').slideUp();
+                $('.rocket').animate({
+                    top: -1000
+                },2000,function(){
+                    $('#activated').slideDown();
+                });
+            },
+            contentType: "application/json"
+        });
+    }else if(code==3){
+        var values = $('#values').val();
+        var data = {valueArr:values.split(",")};
+        $.post(genPath,data,function(data){
+            $('#activator').slideUp();
+            $('.rocket').animate({
+                top: -1000
+            },2000,function(){
+                $('#activated').slideDown();
+            });
+        })
+    }
+    
+}
+function togglePanel(src){
+    $('#'+src).slideToggle('slow');
+}
+function next(){
+    $.get('/next',function(data){
+        $('.output').animate({
+            right:-500
+        },500,function(){
+            $('.output').html(data.next);
+            $('.output').css({right:'120%'});
+            $('.output').animate({
+                right:'46%'
+            },500);
+        })
+        
+    })
+}
